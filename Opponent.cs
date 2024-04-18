f2e7a5e75d32601ed8e495580263339489035716
Kyle <misenckj@gmail.com>
2024-04-17 21:51:53 -0600

    Trying to make ai stupid

--- Opponent.cs
+++ Opponent.cs
@@ -13,6 +13,10 @@ public partial class Opponent : Area2D
 	// Size of the game window.
 	public Vector2 ScreenSize;
 
+	private float _ballPosition;
+
+	private bool upDirection = true;
+
 	public void Start(Vector2 position)
 	{
 		Position = position;
@@ -57,14 +61,49 @@ public partial class Opponent : Area2D
 	{
 		var ball = GetNodeOrNull<RigidBody2D>("/root/Main/Ball");
 
-		// var velocity = Vector2.Zero;
-
-		// Position += velocity * (float)delta;
-		if (ball != null) 
+		if (ball != null)
 		{
+			// paddle height in px
+			// var paddleSize = GetNode<CollisionShape2D>("CollisionShape2D").Shape.GetRect().Size;
+			double paddleSize = 32;
+			float ballYPosition = ball.Position.Y;
+
+			var lower = ballYPosition - paddleSize;
+			var upper = ballYPosition + paddleSize;
+
+			Vector2 window = new Vector2((float)lower, (float)upper);
+
+			GD.Print("WINDOW Y: ", window.Y);
+			GD.Print("WINDOW X: ", window.X);
+			GD.Print("Ball POSITION: ", ballYPosition);
+			GD.Print("UP DIRECTION?", upDirection);
+
+			// on startup set _ballPosition once
+			if (_ballPosition == 0)
+			{
+				_ballPosition = ballYPosition;
+			}
+
+			if (upDirection && _ballPosition > window.X) 
+			{
+				GD.Print("WE SUB 3");
+				_ballPosition -= 1f;
+			} 
+			else if (!upDirection && _ballPosition < window.Y)
+			{	
+				GD.Print("WE ADD 3");
+				_ballPosition += 1f;
+			}
+			else 
+			{
+				// modifiedYPosition = 0;
+				GD.Print("TOGGLE DIRECTION OF WINDOW");
+                upDirection = !upDirection;
+			}
+
 			Position = new Vector2(
 				x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
-				y: Mathf.Clamp(ball.Position.Y, 0, ScreenSize.Y)
+				y: Mathf.Clamp(_ballPosition, 0, ScreenSize.Y)
 			);
 		}
 	}
@@ -82,11 +121,11 @@ public partial class Opponent : Area2D
 		if (readyPlayer2)
 		{
 			ControlPlayer(delta);
-		} 
-		else 
+		}
+		else
 		{
 			AIControl(delta);
 		}
-		
+
 	}
 }
