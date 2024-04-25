f287d2639bae973f38676c0f2339ab9f5d2791c4
Kyle <misenckj@gmail.com>
2024-04-25 17:56:19 -0600

    Progress on AI by using better prediction

--- Opponent.cs
+++ Opponent.cs
@@ -1,4 +1,3 @@
-// TODO: ORGANIZE CODE
 using Godot;
 
 public partial class Opponent : Area2D
@@ -20,19 +19,7 @@ public partial class Opponent : Area2D
 	private bool upDirection = true;
 	private float adjustedBallPosition;
 
-	private float difficulty = 0.10F;
-
-	Prediction prediction = new Prediction();
-
-	public class Prediction
-	{
-		public float DX {get;set;} = 0;
-		public float DY {get;set;} = 0;
-		public float X {get;set;} = 0;
-		public float Y {get;set;} = 0;
-		public string Direction {get;set;} = "";
-		public int Since {get;set;} = 0;
-	}
+	// private float difficulty = 0.10F;
 
 	public struct Level {
 		public double AiReaction {get;set;}
@@ -65,82 +52,6 @@ public partial class Opponent : Area2D
 		new Level(1.7, 190),
 		new Level(1.8, 200)
 	};
-
-	public struct Coordinate {
-		public float X {get;set;} = 0;
-		public float Y {get;set;} = 0;
-		public string Direction {get;set;} = "";
-
-		public Coordinate(float x, float y, string d)
-		{
-			X = x;
-			Y = y;
-			Direction = d;
-		}
-	}
-
-	private Coordinate Intercept(float x1, float y1, float x2, float y2, string d)
-	{
-		var denom = x2-x1 - (y2-y1);
-		if (denom != 0) {
-			var ua = (y1 - x1) / denom;
-			if ((ua >= 0) && (ua <= 1)) {
-				var ub = (((x2-x1) * y1) - ((y2-y1) * (x1))) / denom;
-				if ((ub >= 0) && (ub <= 1)) {
-					var x = x1 + (ua * (x2-x1));
-					var y = y1 + (ua * (y2-y1));
-					return new Coordinate(x, y, d);
-				}
-			}
-		}
-		GD.Print("RETURNING EMPTY COORD");
-		return new Coordinate(0,0,"NONE");
-	}
-
-	private Coordinate BallIntercept(ref Ball ball, float nx, float ny)
-	{
-		Coordinate pt = new Coordinate(0,0,"NONE");
-		if(nx < 0)
-		{
-			pt = Intercept(
-					ball.Position.X,
-					ball.Position.Y,
-					ball.Position.X + nx,
-					ball.Position.Y + ny,
-					"right");
-		}
-		else if (nx > 0)
-		{
-			pt = Intercept(
-					ball.Position.X,
-					ball.Position.Y,
-					ball.Position.X + nx,
-					ball.Position.Y + ny,
-					"left");
-		}
-		if (pt.Direction == "NONE")
-		{
-			if (ny < 0)
-			{
-				pt = Intercept(
-						ball.Position.X,
-						ball.Position.Y,
-						ball.Position.X + nx,
-						ball.Position.Y + ny,
-						"bottom");
-			} 
-			else if (ny > 0)
-			{
-				pt = Intercept(
-						ball.Position.X,
-						ball.Position.Y,
-						ball.Position.X + nx,
-						ball.Position.Y + ny,
-						"top");
-			}
-		}
-		return pt;
-	}
 
 	public void Start(Vector2 position)
 	{
@@ -202,58 +113,27 @@ public partial class Opponent : Area2D
 		);
 	}
 
-	private void Predict(double delta, ref Ball ball)
+	private Vector2 PredictBallPosition(Vector2 currentPosition, Vector2 currentVelocity)
 	{
-		// Only predict when ball has changed directions or passed a timeout
-		if ((prediction.DX * ball.LinearVelocity.X > 0) &&
-			(prediction.DY * ball.LinearVelocity.Y > 0) &&
-			(prediction.Since < levels[0].AiError))
-			{
-				GD.Print("What is delta for Since: ", delta);
-				prediction.Since += (int)delta;
-				return;
-			}
-		
-		// Determine ball interception point
-		var pt = BallIntercept(ref ball, ball.LinearVelocity.X * 10, ball.LinearVelocity.Y * 10);
-		if (pt.Direction != "NONE")
-		{
-			ScreenSize = GetViewportRect().Size;
-			var top = 0;
-			var bottom =  ScreenSize.Y;
+		// Calculate the time to intercept the ball
+		// Speed should be the AiError
+		float timeToIntercept = Mathf.Abs(currentPosition.Y - Position.Y) / Speed;
 
-			while((pt.Y < top) || (pt.Y > bottom))
-			{
-				if(pt.Y < top) {
-					pt.Y = top + (top - pt.Y); 
-				}
-				else if (pt.Y > bottom)
-				{
-					pt.Y = top + (bottom - top) - (pt.Y - bottom);
-				}
-			}
-			prediction.X = pt.X;
-			prediction.Y = pt.Y;
-			prediction.Direction = pt.Direction;
-		} else {
-			prediction.Direction = "NONE";
-		}
+		// Predict the ball's future location based on velocity
+		Vector2 predictedPosition = currentPosition + currentVelocity * timeToIntercept;
 
-		if (prediction.Direction != "NONE")
-		{
-			prediction.Since = 0;
-			prediction.DX = ball.LinearVelocity.X;
-			prediction.DY = ball.LinearVelocity.Y;
-		}
+		// var closeness = Position.X - currentPosition.X;
+		// GD.Print("CLOSENESS: ", closeness);
+		// var error = levels[0].AiError * closeness;
+		// GD.Print("ERROR * CLOSENESS: ", error);
+		// prediction.Y += (float)GD.RandRange(-error, error);
 
-		var closeness = Position.X - ball.Position.X;
-		GD.Print("CLOSENESS: ", closeness);
-		var error = levels[0].AiError * closeness;
-		GD.Print("ERROR * CLOSENESS: ", error);
-		prediction.Y += (float)GD.RandRange(-error, error);
+		// predictedPosition.Y += (float)GD.RandRange(-error, error);
+
+		return predictedPosition;
 	}
 
-	private void NewAIControl(double delta)
+	private void NewAIControl(double delta) 
 	{
 		var ball = GetNodeOrNull<Ball>("/root/Main/Ball");
 
@@ -267,22 +147,18 @@ public partial class Opponent : Area2D
 				return;
 			}
 
-			Predict(delta, ref ball);
-
-			if(prediction.Direction != "NONE")
+			Vector2 predictedLocation = PredictBallPosition(ball.Position, ball.LinearVelocity);
+			if (predictedLocation.Y < Position.Y)
 			{
-				if (prediction.Y < Position.Y)
-				{
-					MoveUp(ref velocity);
-				}
-				else if (prediction.Y > Position.Y)
-				{
-					MoveDown(ref velocity);
-				}
-				else 
-				{
-					StopMoving(ref velocity);
-				}
+				MoveUp(ref velocity);
+			}
+			else if (predictedLocation.Y > Position.Y)
+			{
+				MoveDown(ref velocity);
+			}
+			else 
+			{
+				StopMoving(ref velocity);
 			}
 
 			if (velocity.Length() > 0)
